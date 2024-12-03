f4eb98cd320288d7ba0067a3832aeaa8b187c6a0
MatteoDiMario96 <dm.matteo96@live.it>
2024-12-03 19:12:15 +0100

    Added the dark/light mode with them logic

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,10 +1,30 @@
+import { useEffect, useState } from "react"
+
 export default function Header() {
+
+    const [isDarkMode, setIsDarkMode] = useState(false);
+
+    useEffect(() => {
+        if (isDarkMode) {
+            document.body.classList.remove('light-theme');
+            document.body.classList.add('dark-theme');
+        } else {
+            document.body.classList.remove('dark-theme');
+            document.body.classList.add('light-theme');
+        }
+    }, [isDarkMode])
 
     return (
         <header className="flex-centered">
             <h1>
                 TIC-TAC-TOE REACT
             </h1>
+            <div className="column">
+                <span>{isDarkMode ? "dark-mode".toUpperCase() : "light-mode".toUpperCase()}</span>
+                <button onClick={() => setIsDarkMode((darkMode) => !darkMode)} >
+                    <i className="fa-solid fa-circle-half-stroke"></i>
+                </button>
+            </div>
         </header>
     )
 }
